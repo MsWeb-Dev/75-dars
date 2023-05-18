cd0821742e94d6cf7abdec67fc7c773930cfcacb
SHAXNOZA_DEV <mswebdev175@gmail.com>
2023-05-18 14:14:34 +0500

    75-dars uchun fayllar yaratildi

---
+++ app.js
@@ -0,0 +1,8 @@
+// let  student = ["Shaxnoza","Odina","Javoohir"]
+// let students = ["Alisher"]
+// student.includes(students) ? console.log(`${students} bugun darsda bor`) :  console.log(`${students} bugun darsda yoq`)
+
+
+// const car = ()=>{
+// class 
+// }
