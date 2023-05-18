9ba9d218c25a1f2c719d23b4e9a0fbf531ae7c0d
SHAXNOZA_DEV <mswebdev175@gmail.com>
2023-05-18 15:06:01 +0500

    Default parametr rest operator trenery operator includes class constructor va chart js haqida

--- app.js
+++ app.js
@@ -1,8 +1,86 @@
-// let  student = ["Shaxnoza","Odina","Javoohir"]
-// let students = ["Alisher"]
-// student.includes(students) ? console.log(`${students} bugun darsda bor`) :  console.log(`${students} bugun darsda yoq`)
+//default parametr
 
+// function coder (name){
+//     console.log(`${name} is the best coder`)
 
-// const car = ()=>{
-// class 
 // }
+
+// coder("Shaxnoza")
+// function coder (name="Shaxnoza"){
+//     console.log(`${name} is the best coder`)
+
+// }
+
+// coder("SHag'zod")
+
+// function addNumber (num1=0,num2=0){
+//     console.log(num1+num2)
+// }
+// addNumber(9,8)
+//________________________________
+///for ofni  objectlarda qollash
+// const student = [
+//     {name:"Aziz",age:16,job:"Developer"},
+//     {name:"Aziza",age:17,job:"Teacher"},
+//     {name:"Azizjon",age:18,job:"Customer"}
+// ]
+
+// for(let students of student){
+//     console.log(`${students.name} yoshi ${students.age} va u ${students.job} kasbiga qiziqadi`)
+// }
+//____________________________
+//Rest operator
+// function number (...num){
+// for(let numb of num){
+//     // console.log(numb)
+//     // console.log(numb<50)
+//     // numb<50 ? console.log(numb) : console.log()
+//     // numb<40 ? console.log(numb) : console.log()
+//     numb<70 ? console.log(numb) : console.log()
+//     numb<75 ? console.log(numb) : console.log()
+
+
+// }
+// }
+// number(45,56,76) //bu yerda 1ta parametr bolganligi uchun faqat 1ta qiymat qaytaradi 
+//rest operatoridan foydalanib 1ta parametr yordamida barcha qiymatlarni chiqarish mumkin
+//buning uchun num parametri oldiga 3ta nuqta qoyiladi ...num
+
+
+//___________rest va trenary👆🏻_______________________
+
+
+//includes yani array ichidan qidirish
+// const arr =[21,12,32,23,43]
+// //bor bolsa true ,yoq bolsa false qaytadi
+// console.log(arr.includes(21))
+
+
+
+// const group8 =["Aziz","Odina","Shaxnoza","Javohir","Diyor"]
+
+// const name = "Alisher"
+
+// group8.includes(name) ? console.log(`${name} bugun darsda qatnashdi`) :console.log(`${name} bugun darsda qatnahmadi`)
+
+//_____________includes_👆🏻______________
+
+//class constructor 
+// class car {
+//     constructor(model,year){
+//      this.model  = model,
+//      this.year = year
+//     }
+// }
+// //ezma usul
+// const car1={
+//     model:"Nexia3",
+//     year:2022
+// }
+// //oson usul
+// const car2 = new car("Malibu",2023)
+// const car3 = new car("Matiz",2016)
+
+// console.log(car1)
+// console.log(car2)
+// console.log(car3)
